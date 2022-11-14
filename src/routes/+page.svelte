852d8df99a193ef1180a47f3f4e8beed6dea5272
Phil <92030086+MrNamazu@users.noreply.github.com>
2022-11-14 13:21:52 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import bgft from '$lib/assets/img/background_ft.svg';
+	import bghead from '$lib/assets/img/bghead.png';
 	import Fulllogo from '$lib/components/fulllogo.svelte';
 	import Button from '$lib/components/modules/button.svelte';
 	import { Fullpage, FullpageSection, FullpageSlide } from 'svelte-fullpage';
@@ -9,7 +11,8 @@
 </svelte:head>
 
 <Fullpage>
-	<!-- 	<FullpageSection title="Svelte Fullpage">
+ 	<FullpageSection title="Svelte Fullpage">
+
 		<div class="container text-center">
 			<div class="row">
 				<div class="col">
@@ -21,41 +24,12 @@
 		</div>
 	</FullpageSection>
 
-	<FullpageSection title="Features" class="bg-info">
-		<FullpageSlide title="slides">
-			<div class="container text-center">
-				<div class="row">
-					<div class="col">
-						<h1>Slides</h1>
-						<p>Slide 1</p>
-					</div>
-				</div>
-			</div>
-		</FullpageSlide>
-		<FullpageSlide title="arrows" class="bg-danger">
-			<div class="container text-center">
-				<div class="row">
-					<div class="col">
-						<h1>Slides</h1>
-						<p>Slide2</p>
-					</div>
-				</div>
-			</div>
-		</FullpageSlide>
-		<FullpageSlide title="drag" class="bg-success">
-			<div class="container text-center">
-				<div class="row">
-					<div class="col">
-						<h1>Slides</h1>
-						<p>Slide 3</p>
-					</div>
-				</div>
-			</div>
-		</FullpageSlide>
-	</FullpageSection> -->
-
 	<FullpageSection>
-		<div class="flex flex-col relative w-full h-full mb-32">
+
+		<div class="flex flex-col relative w-full h-full mb-32">			
+			<div class="flex w-full">
+				<h1>Get in Touch</h1>
+			</div>
 			<footer class="absolute bottom-0 w-full flex flex-col items-center">
 				<div class="flex items-center justify-between w-[80%]">
 					<div><Fulllogo /></div>
@@ -77,26 +51,26 @@
 							<i class="fa-brands fa-square-github text-3xl mr-2"></i>
 							<i class="fa-brands fa-square-twitter text-3xl"></i>
 						</div>
-						<div class="text-gray-400 text-sm mt-4">© All rights reserved. Philstack.de 2022</div>
+						<div class="text-gray-300 text-sm mt-4">© All rights reserved. Philstack.de 2021 - 2022</div>
 					</div>
 					<div>
 						<div class="font-bold uppercase mb-3">Adresse:</div>
 						<div>
-							<p class="text-gray-400 text-sm">Neu-Ulmer Str. 19</p>
-							<p class="text-gray-400 text-sm">98617 Meiningen</p>
+							<p class="text-gray-300 text-sm">Neu-Ulmer Str. 19</p>
+							<p class="text-gray-300 text-sm">98617 Meiningen</p>
 						</div>
 					</div>
 					<div>
 						<div class="font-bold uppercase mb-3">Kontakt:</div>
 						<div>
-							<p class="text-gray-400 text-sm">01590/6302212</p>
-							<p class="text-gray-400 text-sm">Stehle@Mailbox.org</p>
+							<p class="text-gray-300 text-sm">01590/6302212</p>
+							<p class="text-gray-300 text-sm">Stehle@Mailbox.org</p>
 						</div>
 					</div>
-					<div>
+					<div class="mr-36">
 						<div class="font-bold uppercase mb-3">Links:</div>
-						<div><a href="/" class="text-gray-400 text-sm">Home</a></div>
-						<div><a href="/" class="text-gray-400 text-sm">Blog</a></div>
+						<div><a href="/" class="text-gray-300 text-sm">Home</a></div>
+						<div><a href="/" class="text-gray-300 text-sm">Blog</a></div>
 					</div>
 				</div>
 			</footer>
